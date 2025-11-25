55c72e3d69e23ea158346a4ff5b1d545eba310bb
MaTor271 <144963680+mator271@users.noreply.github.com>
2025-11-25 10:40:24 -0600

    agregar librerias

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var stylus = require('stylus');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/crypting');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(stylus.middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+//app.use('/api/hash', usersRouter);//    Ver que pedo
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
