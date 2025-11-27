14be4a5af26475f0010f603d223a1cb74e1e07b7
MaTor271 <144963680+MaTor271@users.noreply.github.com>
2025-11-27 10:14:20 -0600

    función hash SHA-256

--- controllers/crypto.controller.js
+++ controllers/crypto.controller.js
@@ -1,14 +1,21 @@
-import {getSHA256Hash} from "boring-webcrypto-sha256";
 
-const lockData = async (req, res) => {
-    try {
-        const clave =
-        const data = req.body;
+const crypto = require('crypto');
 
-        const cryptedData = crypto.AES.encrypt(data);
+const getSHA256Hash =  (req, res) => {
+  try {
+    const { data } = req.body;
+    if (!data) {
+      return res.status(400).json({ error: 'Data is required' });
     }
-}
+    const hash = crypto.createHash('sha256').update(data).digest('hex');
+    return res.status(200).json({ hash });
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+  
+};
 
 module.exports = {
-
+    getSHA256Hash,
+    encriptData
 }
