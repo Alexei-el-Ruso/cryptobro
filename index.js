bcf69a4b5cf8a2304c62f429865d2031eda2574e
Alexei-el-Ruso <alexei-ruso@proton.me>
2025-11-27 18:27:36 -0600

    MIERDA, NI SE CONECTA BIEN, PERO YA MEDIO FUNCIONA!!

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //  Importaciones
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const http = require("http");
 
 const app = express()
 app.use(cors())
@@ -12,3 +13,9 @@ app.get("/", (req, res) => {
 
 const cryptoRoute = require(`./routes/crypto.route`);
 app.use("/api/crypto", cryptoRoute);
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
