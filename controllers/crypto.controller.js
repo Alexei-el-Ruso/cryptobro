5b2db9aa0a6e166fee106053b4e0dd46bfb16421
MaTor271 <144963680+MaTor271@users.noreply.github.com>
2025-12-02 14:05:07 -0600

    aes-256-cbc

--- controllers/crypto.controller.js
+++ controllers/crypto.controller.js
@@ -38,14 +38,59 @@ const postArgon2Hash = (req, res) => {
     });
 
   } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: 'Internal Server Error' });
-  }
+      console.error("Internal catch:", error);
+      return res.status(500).json({ 
+      error: 'Internal Server Error',
+      detail: error.message
+  });
+}
 };
 
+const ENC_KEY = "bf3c199c2470cb477d907b1e0917c17b"; // 32 bytes in hex
+const IV = "5183666c72eec9e4"; // 16 bytes in hex
 
+const postAESEncrypt = (req, res) => {
+  try {
+    const { data } = req.body;
+    if (!data) {
+      return res.status(400).json({ error: 'Data is required' });
+    }
+
+    let cipher = crypto.createCipheriv('aes-256-cbc', ENC_KEY, IV);
+    let encrypted = cipher.update(data, 'utf8', 'base64');
+    encrypted += cipher.final('base64');
+    
+    return res.status(200).json({ encrypted });
+  } catch (error) {
+    console.error('Encryption error:', error);
+    return res.status(500).json({ error: 'Encryption failed', detail: error.message });
+  }
+}
+
+const postAESDesencrypt = (req, res) => {
+  try {
+    const { data } = req.body;
+    if (!data) {
+      return res.status(400).json({ error: 'Data is required' });
+    }
+
+    let decipher = crypto.createDecipheriv('aes-256-cbc', ENC_KEY, IV);
+    let decrypted = decipher.update(data, 'base64', 'utf8');
+    decrypted += decipher.final('utf8');
+    
+    return res.status(200).json({ decrypted });
+  } catch (error) {
+    console.error('Decryption error:', error);
+    return res.status(500).json({ error: 'Decryption failed', detail: error.message });
+  }
+}
 
 module.exports = {
     postSHA256Hash,
-    postArgon2Hash
+    postArgon2Hash,
+    postAESEncrypt,
+    postAESDesencrypt
 }
+
+
+
